2c8910a43bbb08f79b2230e7a724c35605a69aab
Appw0 <austinpaulpal@gmail.com>
2024-02-26 21:21:01 -0700

    Added ShopDemo.c

---
+++ ShopDemo.c
@@ -0,0 +1,113 @@
+// Got help from reading: https://viewsourcecode.org/snaptoken/kilo/02.enteringnewTermSettingsMode.html
+// Use 'a' and 'd' to schmoove, and q to exit.
+#include <ctype.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+struct termios originalTermSettings;
+
+void disableRawMode() {
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &originalTermSettings);
+}
+
+void enableRawMode() {
+	tcgetattr(STDIN_FILENO, &originalTermSettings);
+	atexit(disableRawMode);
+	
+	struct termios newTermSettings = originalTermSettings;
+	// Disables echo and canonical mode (which usually holds input until Enter is pressed)
+	newTermSettings.c_lflag &= ~(ECHO | ICANON);
+	
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &newTermSettings);
+}
+
+int main() {
+	int i, x, y, selected = 0;
+	char c = '\0';
+	char itemSprites[][5][6] = {
+		{
+			"     ",
+			" \\ / ",
+			" |O| ",
+			" / \\ ",
+			"     "
+		},
+		{
+			"     ",
+			" [J=/",
+			" [%&]",
+			" [&%]",
+			" \\--/"
+		},
+		{
+			"     ",
+			" >==R",
+			"   []",
+			"     ",
+			"     "
+		}
+	};
+	
+	char itemNames[][32] = {
+		"Fusion Mine",
+		"Plasma Clip",
+		"T77 Point-pistol"
+	};
+	char itemDescriptions[][512] = {
+		"An old, fusion-based anti-personnel mine. Extremely outdated and questionably operable. Who knows what model it is. These things were so common once that entire an field of engineering sprouted up to find these specific models deep beneath layers of soil and sand.",
+		"Standard issue plasma clip. Filled with enough deuterium to ignite a stellerator worthy of a starhopper. But you're not going to use it for that are you?",
+		"The most basic of point-pistols. Balanced well with little recoil. It can blast pinholes straight through solid, 3 inch steel. Unfortunately, it's so ubiquitous that just about any idiot's personal shield can deflect it with ease."
+	};
+	char keyEscape[8];
+	char spacers[4] = "  <>";
+	char descriptionLine[64];
+	
+	enableRawMode();
+	
+	do {
+		if (c == 27) {
+			memset(keyEscape, '\0', 8);
+			read(STDIN_FILENO, &keyEscape, 8);
+			printf(":: %s\n", keyEscape);
+			
+			if (strcmp(keyEscape, "[C") == 0) {
+				selected = ++selected > 2 ? 2 : selected;
+			} else if (strcmp(keyEscape, "[D") == 0) {
+				selected = --selected < 0 ? 0 : selected;
+			}
+		} else if (iscntrl(c)) {
+			printf("%d\n", c);
+		} else {
+			printf("%d ('%c')\n", c, c);
+		}
+		
+		printf("\x1b[2J"); // Clear screen
+		printf("\x1b[H");
+		
+		printf("\n\n  ░▀█▀░█▄█▒██▀░░░▄▀▀░█▄█░▄▀▄▒█▀▄▒█▀▄▒██▀\n  ░▒█▒▒█▒█░█▄▄▒░▒▄██▒█▒█░▀▄▀░█▀▒░█▀▒░█▄▄\n\n");
+		
+		for (y = 0; y < 5; y++) {
+			printf("     ");
+			for (x = 0; x < 3; x++) {
+				int selectedSpacer = 2*(x == selected);
+				printf("%c %s %c  ", spacers[selectedSpacer], itemSprites[x][y], spacers[selectedSpacer + 1]);
+			}
+			printf("\n");
+		}
+		
+		printf("\n           -- %s --\n", itemNames[selected]);
+		printf("\n%s\n", itemDescriptions[selected]);
+		
+		// printf("%d\n", (int)((double)strlen(itemDescriptions[selected])/64+0.5));
+		
+		// for (i = 0; i < 1; i++) {
+			// strncat(descriptionLine, itemDescriptions[selected] + 64*i, 64);
+			// printf("%s", descriptionLine);
+		// }
+	} while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	return 0;
+}
