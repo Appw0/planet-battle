d047f782f0deccfbdc5b6087cf8682c3133d1018
KingCam16 <43072336+KingCam77@users.noreply.github.com>
2024-02-29 11:07:58 -0700

    Update Movement.c

--- Movement.c
+++ Movement.c
@@ -4,6 +4,7 @@ void moveActor(int actorNum, int dir) {
 
   int xpos=actors[actorNum][actorX];
   int ypos=actors[actorNum][actorY];
+  int attackDone;
   
   // Dec the actor move cooldown
   if (actors[actorNum][actorMoveCool] == 0) {
@@ -12,24 +13,28 @@ void moveActor(int actorNum, int dir) {
     } else {
       actors[actorNum][actorMoveCool] = 0;
     }
-    
-    
-    // Move in direction
-  	if ((dir==1) && (map[ypos-1][xpos]>Barrier)) {
-  		actors[actorNum][actorY]=actors[actorNum][actorY]-1;
-  	}
-    if ((dir==2) && (map[ypos][xpos-1]>Barrier)) {
-  		actors[actorNum][actorX]=actors[actorNum][actorX]-1;
-  	}
-    if ((dir==3) && (map[ypos+1][xpos]>Barrier)) {
-  		actors[actorNum][actorY]=actors[actorNum][actorY]+1;
-  	}
-    if ((dir==4) && (map[ypos][xpos+1]>Barrier)) {
-  		actors[actorNum][actorX]=actors[actorNum][actorX]+1;
-  	}
-  } else {
-    actors[actorNum][actorMoveCool]=actors[actorNum][actorMoveCool]-1;
-  }
+      attackDone = Attack(dir,xpos,ypos,actorNum);
+      printf("%d",attackDone);
+      if ( attackDone == 0 ) {
+        
+        // Move in direction
+        if ((dir==1) && (map[ypos-1][xpos]>Barrier)) {
+      		actors[actorNum][actorY]=actors[actorNum][actorY]-1;
+      	}
+        if ((dir==2) && (map[ypos][xpos-1]>Barrier)) {
+      		actors[actorNum][actorX]=actors[actorNum][actorX]-1;
+      	}
+        if ((dir==3) && (map[ypos+1][xpos]>Barrier)) {
+      		actors[actorNum][actorY]=actors[actorNum][actorY]+1;
+      	}
+        if ((dir==4) && (map[ypos][xpos+1]>Barrier)) {
+      		actors[actorNum][actorX]=actors[actorNum][actorX]+1;
+      	}
+       
+      }
+    } else {
+      actors[actorNum][actorMoveCool]=actors[actorNum][actorMoveCool]-1;
+    }
  
 }
 
