2299f3035d7d6fc7a1f28a41c959f92cce6e4d0b
Appw <austinpaulpal@gmail.com>
2024-04-02 10:29:48 -0700

    Commented Main.c

--- Main.c
+++ Main.c
@@ -1,65 +1,68 @@
 #include "project.h"
 
-char sideText[1000]="This is a test level for development while actual maps get created.\n";
+char sideText[1000]="\0";
 char topText[topTextLength];
 
 int main(int argCount, char* args[]) {
-    srand(time(NULL));
+	srand(time(NULL));
 	enableRawMode();
 	
+	 // If the gamedata fails to load, exit the program with an error code of 1
 	if (!loadData()) return 1;
 	
-	// This function is temporary and should be replaced with loading save data
 	setupDefaultPlayer();
 	
 	if (argCount > 1) {
+		// If an argument has been passed in, interpret it as a level name to load
 		if (!loadLevel(args[1])) return 1;
 	} else {
-		if (!loadLevel("TestLevel")) return 1;
+		// Otherwise load Level1 and play the intro movie
+		if (!loadLevel("Level1")) return 1;
 		introMovie();
 	}
-    
+	
 	int playerHadTurn = 0;
 	char keyCode[8] = "\0";
 	
+	// Initialize the laserMap and draw the screen to clear away the intro movie
 	computeLaserMap();
 	drawScreen();
 	
-  do {
-	  
-	 playerHadTurn = 0;
-	 if (keyCode[0] == 'i') {
-		 manageInventory();
-		 drawScreen();
-   } else if (keyCode[0] == 'h') {
-     drawHelp();
-	 } else if (keyCode[0] == '.') {
-		 playerHadTurn = 1; // pass time
-	 } else if (keyCode[0] == 'f') {
-		 playerHadTurn = playerAimLaser();
-	 } else if (playerMovement(keyCode)) {
-		 playerPickupItems();
-		 playerCheckPortals();
-		 playerHadTurn = 1;
-	 }
-	 
-	 if (playerHadTurn) {
-		 doAI();
-		 computeLaserMap();
-		 setLasersRendered();
-		 drawScreen();
-	 }
-	 
-	 if (playerDied) {
-		 printDramatic($lred "\n\n     You are dead.");
-		 sleepMs(1500);
-		 printDramatic($white "\n\n     It's " $lblue "cold" $white " and " $lmagenta "clammy" $white " here.\n\n");
-		 sleepMs(1500);
-		 //printf($lred "\nYou ded\n"); // TODO: actually roll back the game to a saved state
-		 break;
-	 }
-   
-	} while (readKey(keyCode, 8));
-    
-    return 0;
+	do {
+		playerHadTurn = 0; // Will only be 1 if the input was recognized and should advance time
+		
+		if (keyCode[0] == 'i') {
+			manageInventory(); // Load the inventory menu, this blocks until its closed
+			drawScreen(); // Clear it immediately after its closed so things feel responsive
+			// Please note that time is NOT advanced here
+		} else if (keyCode[0] == 'h') {
+			drawHelp(); // Help menu
+		} else if (keyCode[0] == '.') {
+			playerHadTurn = 1; // Wait around and do nothing by advancing time
+		} else if (keyCode[0] == 'f') {
+			playerHadTurn = playerAimLaser(); // Laser menu! Blocks just like manageInventory()
+		} else if (playerMovement(keyCode)) {
+			playerPickupItems(); // The player just moved, so lets vacuum up everthing they walked into
+			playerCheckPortals(); // And make sure they don't have anywhere to be
+			playerHadTurn = 1;
+		}
+		
+		if (playerHadTurn) {
+			doAI();
+			computeLaserMap();
+			setLasersRendered(); // Tells the laserMap that we want all the lasers to dissapate next turn
+			drawScreen();
+		}
+		
+		if (playerDied) {
+			printDramatic($lred "\n\n     You are dead.");
+			sleepMs(1500);
+			printDramatic($white "\n\n     It's " $lblue "cold" $white " and " $lmagenta "clammy" $white " here.\n\n");
+			sleepMs(1500);
+			break;
+		}
+		
+	} while (readKey(keyCode, 8)); // read in 8 characters of the next keyCode pressed
+	
+	return 0;
 }
