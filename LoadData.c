141bdb8f575d2a84482607ae2d85aea1c395a336
Appw0 <austinpaulpal@gmail.com>
2024-04-03 19:10:55 -0700

    Commented LoadData.c
    They're not very thorough comments, but oh well

--- LoadData.c
+++ LoadData.c
@@ -1,5 +1,6 @@
 #include "project.h"
 
+// A structure to store a piece of an INI file
 struct iniEntry {
 	char name[iniMaxNameLength];
 	char keys[iniMaxKeys][iniMaxKeyLength];
@@ -7,8 +8,10 @@ struct iniEntry {
 	int numKeys;
 };
 
+// A map of placeholders characters, to be loaded from a Level file
 char tempMap[mapMaxHeight][mapMaxWidth];
 
+// Places a group of tiles in the real map by placing them where their placeholders are in the tempMap
 void mapTiles(char placeholder, struct tileTypeData* types[], int numTypes) {
 	int x, y;
 	for (y = 0; y < mapHeight; y++) {
@@ -20,6 +23,7 @@ void mapTiles(char placeholder, struct tileTypeData* types[], int numTypes) {
 	}
 }
 
+// Gets all positions of a placeholder character in the tempMap and replace it with a different placeholder
 int getAndSwapPlaceholderTiles(char oldPlaceholder, char newPlaceholder, struct position pos[], int maxPosCount) {
 	int x, y, i = 0;
 	for (y = 0; y < mapHeight; y++) {
@@ -40,6 +44,7 @@ int getAndSwapPlaceholderTiles(char oldPlaceholder, char newPlaceholder, struct 
 	return i;
 }
 
+// Gets a single placeholder character in the tempMap and swaps it with a new one
 int getAndSwapPlaceholderTile(char oldPlaceholder, char newPlaceholder, int* x, int* y) {
 	struct position pos[1];
 	int i = getAndSwapPlaceholderTiles(oldPlaceholder, newPlaceholder, pos, 1);
@@ -48,30 +53,37 @@ int getAndSwapPlaceholderTile(char oldPlaceholder, char newPlaceholder, int* x, 
 	return i;
 }
 
+// Reads in an int from an iniEntry at the specific index if the key matches
 void readInt(struct iniEntry data, int index, char key[], int* value) {
 	if (key == NULL || strcmp(data.keys[index], key) == 0) sscanf(data.values[index], "%d", value);
 }
 
+// Reads in a char from an iniEntry at the specific index if the key matches
 void readChar(struct iniEntry data, int index, char key[], char* value) {
 	if (key == NULL || strcmp(data.keys[index], key) == 0) (*value) = data.values[index][0];
 }
 
+// Reads in a char array from an iniEntry at the specific index if the key matches
 void readStr(struct iniEntry data, int index, char key[], char dest[], int length) {
 	if (key == NULL || strcmp(data.keys[index], key) == 0) strncpy(dest, data.values[index], length);
 }
 
+// Reads in a color int from an iniEntry at the specific index if the key matches
 void readColor(struct iniEntry data, int index, char key[], int* value) {
 	if (key == NULL || strcmp(data.keys[index], key) == 0) (*value) = evalColor(data.values[index]);
 }
 
+// Reads in an itemCategory from an iniEntry at the specific index if the key matches
 void readItemCategory(struct iniEntry data, int index, char key[], int* value) {
 	if (key == NULL || strcmp(data.keys[index], key) == 0) (*value) = evalItemCategory(data.values[index]);
 }
 
+// Reads in an itemType from an iniEntry at the specific index if the key matches
 void readItemType(struct iniEntry data, int index, char key[], struct itemTypeData** value) {
 	if (key == NULL || strcmp(data.keys[index], key) == 0) (*value) = getItemPtr(data.values[index]);
 }
 
+// Reads in an array of itemTypes from an iniEntry at the specific index if the key matches
 int readItemTypes(struct iniEntry data, int index, char key[], struct itemTypeData* types[], int number, int* typesFound) {
 	int i = 0;
 	if (key == NULL || strcmp(data.keys[index], key) == 0) {
@@ -86,10 +98,12 @@ int readItemTypes(struct iniEntry data, int index, char key[], struct itemTypeDa
 	return i;
 }
 
+// Reads in an actorType from an iniEntry at the specific index if the key matches
 void readActorType(struct iniEntry data, int index, char key[], struct actorTypeData** value) {
 	if (key == NULL || strcmp(data.keys[index], key) == 0) (*value) = getActorTypePtr(data.values[index]);
 }
 
+// Reads in an array of actorTypes from an iniEntry at the specific index if the key matches
 int readActorTypes(struct iniEntry data, int index, char key[], struct actorTypeData* types[], int number, int* typesFound) {
 	int i = 0;
 	if (key == NULL || strcmp(data.keys[index], key) == 0) {
@@ -104,10 +118,12 @@ int readActorTypes(struct iniEntry data, int index, char key[], struct actorType
 	return i;
 }
 
+// Reads in a tileType from an iniEntry at the specific index if the key matches
 void readTileType(struct iniEntry data, int index, char key[], struct tileTypeData** value) {
 	if (key == NULL || strcmp(data.keys[index], key) == 0) (*value) = getTileTypePtr(data.values[index]);
 }
 
+// Reads in an array of tileTypes from an iniEntry at the specific index if the key matches
 int readTileTypes(struct iniEntry data, int index, char key[], struct tileTypeData* types[], int number, int* typesFound) {
 	int i = 0;
 	if (key == NULL || strcmp(data.keys[index], key) == 0) {
@@ -122,6 +138,7 @@ int readTileTypes(struct iniEntry data, int index, char key[], struct tileTypeDa
 	return i;
 }
 
+// Reads a tileType with a few defaults and creates it
 void readTileINI(struct iniEntry data) {
 	char tile = ' ';
 	int i, color = black, bgColor = black, blockLaser = 0, blockMove = 0;
@@ -135,6 +152,7 @@ void readTileINI(struct iniEntry data) {
 	createTileType(data.name, tile, color, bgColor, blockMove, blockLaser);
 }
 
+// Reads an itemType with a few defaults and creates it
 void readItemINI(struct iniEntry data) {
 	char displayName[itemMaxNameLength] = "???";
 	int i, category = itemCategoryNone, damage = 0;
@@ -146,6 +164,7 @@ void readItemINI(struct iniEntry data) {
 	createItemType(data.name, displayName, category, damage);
 }
 
+// Reads an actorType with a few defaults and creates it
 void readActorTypeINI(struct iniEntry data) {
 	char displayName[actorMaxNameLength] = "???", tile = ' ';
 	int i, color = black, bgColor = black, spawnPoints = 0, health = 0, slowness = 0;
@@ -163,6 +182,7 @@ void readActorTypeINI(struct iniEntry data) {
 	createActorType(data.name, displayName, tile, color, bgColor, spawnPoints, health, slowness, weapon);
 }
 
+// Reads some level metadata
 void readLevelInfo(struct iniEntry data) {
 	int i;
 	for (i = 0; i < data.numKeys; i++) {
@@ -171,6 +191,7 @@ void readLevelInfo(struct iniEntry data) {
 	}
 }
 
+// Reads the level map from a level INI
 void readLevelMap(struct iniEntry data) {
 	int y, x, mapLargestPossibleWidth;
 	
@@ -194,6 +215,7 @@ void readLevelMap(struct iniEntry data) {
 	}
 }
 
+// Reads an instruction to create an actor at a specific placeholder character
 void readLevelActor(struct iniEntry data) {
 	int i, posCount = 0;
 	char placeholder = 'X', replace = '.';
@@ -210,6 +232,11 @@ void readLevelActor(struct iniEntry data) {
 	createActorRandomPos(type, pos, posCount, 1);
 }
 
+// Reads an instruction to create some random actors at a specific placeholder character
+// This function has a few different ways it can work.
+// If spawnPoints is not specified, it will use the spawnPoints of the level
+// If countMax or countMin is specified, if will only create an amount between the bounds
+// If spawnPoints is specified, this function will use its own unique pool of spawnPoints
 void readLevelActors(struct iniEntry data) {
 	int i, posCount = 0, readTypes = 0, minCount = -1, maxCount = -1, spawnPoints = -1;
 	char placeholder = 'X', replace = '.';
@@ -231,6 +258,7 @@ void readLevelActors(struct iniEntry data) {
 	createActors(types, readTypes, pos, posCount, 1, minCount, maxCount, spawnPoints);
 }
 
+// Reads an instruction to create the player at a specific placeholder character
 void readLevelPlayer(struct iniEntry data) {
 	char replace = '.';
 	int i, x, y;
@@ -241,6 +269,7 @@ void readLevelPlayer(struct iniEntry data) {
 	createPlayer(x, y);
 }
 
+// Reads an instruction to create a portal at a specific placeholder
 void readLevelPortal(struct iniEntry data) {
 	char placeholder = 'X', replace = '.', level[iniMaxNameLength];
 	int i, x, y;
@@ -253,6 +282,8 @@ void readLevelPortal(struct iniEntry data) {
 	createPortal(x, y, level);
 }
 
+
+// Reads the placeholder to tile instructions
 void readLevelTiles(struct iniEntry data) {
 	struct tileTypeData* types[iniMaxListLength];
 	int i, tilesFound = 0;
@@ -264,6 +295,7 @@ void readLevelTiles(struct iniEntry data) {
 }
 
 // TODO: levelItems should not load if the room has been cleared before
+// Reads an instruction to an item in the level
 void readLevelItem(struct iniEntry data) {
 	int i, posCount = 0;
 	char placeholder = 'X', replace = '.';
@@ -280,6 +312,8 @@ void readLevelItem(struct iniEntry data) {
 	createDroppedItemRandomPos(type, pos, posCount);
 }
 
+
+// Reads an instruction to place randomly selected items in the level 
 void readLevelItems(struct iniEntry data) {
 	int i, posCount = 0, readTypes = 0, minCount = 1, maxCount = 1;
 	char placeholder = 'X', replace = '.';
@@ -300,6 +334,7 @@ void readLevelItems(struct iniEntry data) {
 	createDroppedItems(types, readTypes, pos, posCount, maxCount, minCount);
 }
 
+// This function delegates the different data entries in the level files to their respective functions
 void readLevelINI(struct iniEntry data) {
 	if (strcmp(data.name, "info") == 0) {
 		readLevelInfo(data);
@@ -322,6 +357,7 @@ void readLevelINI(struct iniEntry data) {
 	}
 }
 
+// Reads in a datapad, specifically its name and contents
 void readDatapadINI(struct iniEntry data) {
 	char text[datapadTextMaxLength] = "\0";
 	int i;
@@ -337,6 +373,8 @@ void readDatapadINI(struct iniEntry data) {
 	createDatapad(data.name, text);
 }
 
+// This complex function will parse an INI file into an iniEntry struct
+// It then calls the passed in function "readData" with that struct for further, file specific parsing
 int readINI(char fileName[], void(*readData)(struct iniEntry)) {
 	struct iniEntry data;
 	char line[iniMaxLineLength];
@@ -361,7 +399,8 @@ int readINI(char fileName[], void(*readData)(struct iniEntry)) {
 				if (!readFirstValue) {
 					readFirstValue = 1;
 				} else {
-					// TODO: get these functions to return success/failure
+					// TODO: get these functions to return success/failure to stop the game when invalid data is loaded
+					// Current behavior does print out errors already for the user, but it bulldozes right through them
 					readData(data);
 				}
 				data.numKeys = 0;
@@ -398,6 +437,7 @@ int readINI(char fileName[], void(*readData)(struct iniEntry)) {
 }
 
 
+// Loads a level with the specified file name. The .ini suffix is appended automatically and it searches in the ./data directory
 // Name should never be bigger than iniMaxNameLength
 int loadLevel(char name[]) {
 	char path[iniMaxNameLength + 16] = "./data/";
@@ -414,6 +454,7 @@ int loadLevel(char name[]) {
 	return readINI(path, readLevelINI);
 }
 
+// Load the default game data, and return 1 if they were all successful, 0 if not.
 int loadData() {
 	int success = 1;
 	success &= readINI("./data/Tiles.ini", readTileINI);
