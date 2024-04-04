05dbd1ff1c282d522dda7b5da18ed656c050fd9f
Appw0 <austinpaulpal@gmail.com>
2024-04-03 20:09:27 -0700

    Commented TermManage.c

--- TermManage.c
+++ TermManage.c
@@ -1,15 +1,15 @@
-// Got help from reading: https://viewsourcecode.org/snaptoken/kilo/02.enteringnewTermSettingsMode.html
-// q to exit.
-
 #include "project.h"
 
 struct termios originalTermSettings;
 char selectSpacers[4] = "  ><";
 
+
+// Resets the terminal back to its original settings
 void disableRawMode() {
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &originalTermSettings);
 }
 
+// Configures the terminal to accept keyboard input directly
 void enableRawMode() {
 	tcgetattr(STDIN_FILENO, &originalTermSettings);
 	atexit(disableRawMode);
@@ -21,6 +21,7 @@ void enableRawMode() {
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &newTermSettings);
 }
 
+// Parses int representations of color and effect into an escape sequence
 void printColorBgExtras(int color, int bgColor, int extra) {
 	printf("\033[%d;%d;%dm", extra, color, bgColor + 10);
 }
@@ -33,20 +34,26 @@ void printColor(int color) {
 	printColorBg(color, black);
 }
 
+// Sets the color and effect back to defaults
 void resetColor() {
 	printf("\033[0m");
 }
 
 void clearTerm() {
-	//printf("\e[;H");
-	printf("\e[1;1H\e[2J"); // Clear Screen
+	printf("\e[1;1H\e[2J"); // Clears the screen and moves the cursor back to 1,1
 }
 
+// Reads a certain number of stdin characters into keyEscape
+// This is the escape sequence of the pressed key
 int readKey(char keyEscape[], int codeLength) {
 	memset(keyEscape, '\0', codeLength);
 	return read(STDIN_FILENO, keyEscape, codeLength);
 }
 
+// Handles the keyboard logic of moving the cursor around a menu,
+// Represented by a pointer to the currently selected index, and the total number of options
+// It is also configured with whether the input should be vertical, and a keyCode to exit the menu
+// Returns whether the menu is still being interacted with
 int menuHandleInput(int* selected, char keyCode[], int optionNum, int vertical, char exitCode) {
 	if (!vertical && (keyCode[0] == 'd' || strcmp(keyCode, "\e[C") == 0) ||
 		vertical && (keyCode[0] == 's' || strcmp(keyCode, "\e[B") == 0)) {
@@ -60,6 +67,12 @@ int menuHandleInput(int* selected, char keyCode[], int optionNum, int vertical, 
 	return 1;
 }
 
+// Takes over the screen and replaces it with a menu defined by a couple variables
+// text: the text to print before the menu
+// options: a matrix with the text of each option
+// moreInfo: a matrix with more text that will display when an option is hovered over
+// optionNum: the number of options
+// vertical: whether the menu should be vertical or not
 int askWithTextMenu(char text[], char options[][32], char moreInfo[][512], int optionNum, int vertical) {
 	int i, selected = 0;
 	char keyCode[8] = "\0";
@@ -84,6 +97,7 @@ int askWithTextMenu(char text[], char options[][32], char moreInfo[][512], int o
 }
 
 // TODO: Update/make new function to use item/sprite IDs instead of passing an entire array
+// An unused function that can print a menu where the user can select between a number of cool ascii images
 int askWithSpriteMenu(char text[], char sprites[][5][6], char moreInfo[][512], int optionNum) {
 	int x, y, selected = 0;
 	char keyCode[8] = "\0";
@@ -102,6 +116,7 @@ int askWithSpriteMenu(char text[], char sprites[][5][6], char moreInfo[][512], i
 	} while(readKey(keyCode, 8) && menuHandleInput(&selected, keyCode, optionNum, 0, '\n'));
 }
 
+// Prints out text dramatically, by pausing between each letter, word, line, and sentence
 void printDramatic(char text[]) {
 	setbuf(stdout, NULL);
 	
