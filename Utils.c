ef5082954b9188453782b5cbcfa941ba520aabd3
Appw0 <austinpaulpal@gmail.com>
2024-04-04 20:38:15 -0700

    Commented Utils.c

--- Utils.c
+++ Utils.c
@@ -2,20 +2,23 @@
 
 struct timespec requested, remaining;
 
+
+// Sleeps for a certain number of milliseconds by using nanosleep
 void sleepMs(int millis) {
 	double fractional, integral;
 	fractional = modf(millis/1000.0d, &integral);
     requested.tv_sec = integral;
     requested.tv_nsec = fractional * 1000000000;
-	//printf("\n%ld:%ld-", requested.tv_sec, requested.tv_nsec);
     
     nanosleep(&requested, &remaining);
 }
 
+// A human readable shorthand for testing an item's name
 int itemNameIs(struct itemTypeData type, char name[]) {
 	return strcmp(type.name, name) == 0;
 }
 
+// Returns the droppedItemID of an item at the specified coordinates, or -1 if nothing is found
 int getDroppedItemAtXY(int x, int y) {
 	int id;
 	for (id = 0; id < droppedItemsMaxCount; id++) {
@@ -26,6 +29,7 @@ int getDroppedItemAtXY(int x, int y) {
 	return -1;
 }
 
+// Returns the next free droppedItemID, or -1 if there are too many 
 int getNextDroppedItemID() {
 	int id;
 	for (id = 0; id < droppedItemsMaxCount; id++) {
@@ -37,7 +41,8 @@ int getNextDroppedItemID() {
 	return -1;
 }
 
-
+// Returns the actorID of an actor at the specified coordinates, or -1 if none are there
+// Also has a flag for finding dead actors too
 int getAnyActorAtXY(int x, int y, int deadAllowed) {
 	int id;
 	for (id = 0; id < actorsCreated; id++) { 
@@ -48,25 +53,30 @@ int getAnyActorAtXY(int x, int y, int deadAllowed) {
 	return -1;
 }
 
+// Shorthand if you're only searching for living actors
 int getActorAtXY(int x, int y) {
 	return getAnyActorAtXY(x, y, 0);
 }
 
+// A variant of the above for position structs
 int getActorAt(struct position pos) {
 	return getActorAtXY(pos.x, pos.y);
 }
 
+// The opposite of getActorAtXY, copies the actors x and y into the passed locations
 void getActorXY(int actorID, int* x, int* y) {
 	(*x) = actors[actorID].x;
 	(*y) = actors[actorID].y;
 }
 
+// A variant of the above for positions structs
 struct position getActorPosition(int actorID) {
 	struct position pos;
 	getActorXY(actorID, &pos.x, &pos.y);
 	return pos;
 }
 
+// Returns the player actors actorID. If doesn't find the player, something is very wrong
 int getPlayerID() {
 	int i;
 	for (i = 0; i < actorsCreated; i++) {
@@ -78,30 +88,37 @@ int getPlayerID() {
 	return 0;
 }
 
+// Human readable shorthand for figuring out of this actor is the player
 int isActorPlayer(int actorID) {
 	return actors[actorID].type == getActorTypePtr("player");
 }
 
+// Human readable shorthand for determining if this actor is dead
 int isActorDead(int actorID) {
 	return actors[actorID].type == getActorTypePtr("dead");
 }
 
+// Returns if this actor is a living NPC
 int actorHasAI(int actorID) {
 	return !isActorPlayer(actorID) && !isActorDead(actorID);
 }
 
+// Useful for checking the output of a function which can return an invalid actor ID to indicate an error
 int isValidActorID(int actorID) {
 	return actorID > -1 && actorID < actorsCreated;
 }
 
+// Similar to the above, but for droppedItemIDs
 int isValidDroppedItemID(int id) {
 	return id > -1 && id < droppedItemsMaxCount;
 }
 
+// Percents are hard
 int percent(int num, int outOf) {
   return roundf(((float)num/(float)outOf)*(float)100);
 }
 
+// Adds to position structs
 struct position posAdd(struct position pos1, struct position pos2) {
 	struct position sum = {0, 0};
 	sum.x = pos1.x + pos2.x;
@@ -109,17 +126,20 @@ struct position posAdd(struct position pos1, struct position pos2) {
 	return sum;
 }
 
+// Subtracts two positions structs, as pos1 - pos2
 struct position posSubtract(struct position pos1, struct position pos2) {
 	pos2.x = -pos2.x;
 	pos2.y = -pos2.y;
 	return posAdd(pos1, pos2);
 }
 
+// Finds the distance between two position structs
 float posDistance(struct position pos1, struct position pos2) {
 	struct position dif = posSubtract(pos1, pos2);
 	return sqrt(pow(dif.x, 2) + pow(dif.y, 2));
 }
 
+// Turns a direction int into a position struct representing it as a relative position of unit length
 struct position directionToPos(int direction) {
 	struct position pos;
 	pos.x = 0;
@@ -143,6 +163,7 @@ struct position directionToPos(int direction) {
 	return pos;
 }
 
+// Returns an arrow-like character that points in the specified direction
 char directionToChar(int direction) {
 	switch (direction) {
 		case north:
@@ -158,20 +179,24 @@ char directionToChar(int direction) {
 	}
 }
 
+// RNG!
 int randomDirection() {
 	return rand()%numDirections;
 }
 
+// Turns a direction int into separate x and y variables
 int directionToXY(int direction, int* x, int* y) {
 	struct position pos = directionToPos(direction);
 	(*x) = pos.x;
 	(*y) = pos.y;
 }
 
+// Checks to see if things are getting out of hand
 int isXYOnMap(int x, int y) {
 	return x > 0 && x < mapWidth && y > 0 && y < mapHeight;
 }
 
+// A variant of above for position structs
 int isPosOnMap(struct position pos) {
 	return isXYOnMap(pos.x, pos.y);
 }
@@ -197,6 +222,7 @@ int laserRaycast(int x, int y, int direction, int actorHitIDs[], int maxHits) {
 	return i;
 }
 
+// A big amalgam that turns a color string into a color int
 int evalColor(char color[]) {
 	if (strcmp(color, "black") == 0) {
 		return black;
@@ -236,6 +262,7 @@ int evalColor(char color[]) {
 	}
 }
 
+// A horrible function that turns a category string into a category int
 int evalItemCategory(char category[]) {
 	if (strcmp(category, "none") == 0) {
 		return itemCategoryNone;
